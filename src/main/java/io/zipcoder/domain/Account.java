1a9f624c6e655cbe85d13d83011592a592f3ae67
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-09 11:22:14 -0400

    saving

--- src/main/java/io/zipcoder/domain/Account.java
+++ src/main/java/io/zipcoder/domain/Account.java
@@ -1,4 +1,69 @@
 package io.zipcoder.domain;
 
+import io.zipcoder.Enums.AccountType;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Account {
+
+    private Long id;
+
+    private AccountType type;
+
+    private String nickname;
+
+    private Integer rewards;
+
+    private Double balance;
+
+    private Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type.name();
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(Integer rewards) {
+        this.rewards = rewards;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
