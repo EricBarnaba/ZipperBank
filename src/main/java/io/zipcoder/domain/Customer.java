ee8ef70332be0467260d7d6101f2476898d1838a
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-09 10:57:46 -0400

    saving

--- src/main/java/io/zipcoder/domain/Customer.java
+++ src/main/java/io/zipcoder/domain/Customer.java
@@ -8,7 +8,35 @@ public class Customer {
     private String lastName;
     private Set<Address> addresses;
 
-    //
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
 }
