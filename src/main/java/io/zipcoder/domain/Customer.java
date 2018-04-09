cd980d3db147a221cda0142c60fe11ec7e56d762
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-6.local>
2018-04-09 10:47:43 -0400

    added customer and address

---
+++ src/main/java/io/zipcoder/domain/Customer.java
@@ -0,0 +1,14 @@
+package io.zipcoder.domain;
+
+import java.util.Set;
+
+public class Customer {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Set<Address> addresses;
+
+    //
+
+
+}
