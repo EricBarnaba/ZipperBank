33e952a3ad24b65ddd04404e152611e3f170716a
Anthony Jordan <Kain235@gmail.com>
2018-04-09 11:10:00 -0400

    Added string names to enums

--- src/main/java/io/zipcoder/Enums/Status.java
+++ src/main/java/io/zipcoder/Enums/Status.java
@@ -1,8 +1,14 @@
 package io.zipcoder.Enums;
 
 public enum Status {
-    PENDING,
-    CANCELLED,
-    COMPLETED,
-    RECURRING
+    PENDING("Pending"),
+    CANCELLED("Cancelled"),
+    COMPLETED("Completed"),
+    RECURRING("Recurring");
+
+    private String name;
+
+    Status(String name){
+        this.name = name;
+    }
 }
