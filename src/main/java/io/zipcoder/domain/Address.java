f3dcb0026c9083d1ebfc37523bcd666e7ea12d26
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-6.local>
2018-04-09 10:55:12 -0400

    created fields in address and customer objs

--- src/main/java/io/zipcoder/domain/Address.java
+++ src/main/java/io/zipcoder/domain/Address.java
@@ -1,4 +1,10 @@
 package io.zipcoder.domain;
 
 public class Address {
+    private Long id;
+    private String streetNumber;
+    private String streetName;
+    private String city;
+    private String state;
+    private String zip;
 }
