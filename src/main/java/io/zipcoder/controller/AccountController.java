80f630f88537cafbdc5a3deb0351ef2453186001
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-10 21:28:08 -0400

    account controller stubbed out

--- src/main/java/io/zipcoder/controller/AccountController.java
+++ src/main/java/io/zipcoder/controller/AccountController.java
@@ -3,6 +3,7 @@ package io.zipcoder.controller;
 import io.zipcoder.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,59 @@ public class AccountController {
     }
 
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getAccount() {
+    public ResponseEntity<?> getAccount(@PathVariable Long id) {
         return null;
     }
+
+    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateAccount(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteAccount(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/customer", method = RequestMethod.GET)
+    public ResponseEntity<?> getAccountCustomer(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/bills", method = RequestMethod.GET)
+    public ResponseEntity<?> getAccountBills(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/bills", method = RequestMethod.POST)
+    public ResponseEntity<?> createBill(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/deposits", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllDeposits(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/deposits", method = RequestMethod.POST)
+    public ResponseEntity<?> createDeposit(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/withdrawals", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllWithdrawals(@PathVariable Long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}/withdrawals", method = RequestMethod.POST)
+    public ResponseEntity<?> createWithdrawal(@PathVariable Long id) {
+        return null;
+    }
+
+
+
+
+
 
 
 }
